a4502c54408ab102b418edc744c08a421ba0201a
Pappu-kr-Mahto <pappukumar2032001@gmail.com>
2024-01-27 16:17:48 +0530

    vercel changes

--- turo_backend/turo_backend/wsgi.py
+++ turo_backend/turo_backend/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turo_backend.settings')
 
 application = get_wsgi_application()
+app = application
