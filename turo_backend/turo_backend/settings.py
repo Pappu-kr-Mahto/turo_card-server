119a328e0104ecb20054446cabe7334019d386db
Pappu-kr-Mahto <pappukumar2032001@gmail.com>
2024-01-30 18:08:46 +0530

    render deploy

--- turo_backend/turo_backend/settings.py
+++ turo_backend/turo_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +81,25 @@ WSGI_APPLICATION = 'turo_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# DATABASES = {
+#     'default': dj_database_url.parse({
+#         "default" :'postgres://pappu:Xn5eskIvkl2bFW06wHYq3VfemPpgtNPa@dpg-cmseirv109ks73duvidg-a.oregon-postgres.render.com/turo_card_db',
+#         "conn_max_age" :600,
+#     })
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgres://pappu:Xn5eskIvkl2bFW06wHYq3VfemPpgtNPa@dpg-cmseirv109ks73duvidg-a.oregon-postgres.render.com/turo_card_db',
+        conn_max_age=600
+    )
 }
 
 
